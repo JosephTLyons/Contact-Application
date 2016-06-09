38621da3f9c7d7fa182566da9b2a5b13fb0702b3
Joseph Lyons <joe_04_04@yahoo.com>
2016-06-09 06:28:05 -0400

    2nd commit

--- Vector Practice/main.cpp
+++ Vector Practice/main.cpp
@@ -13,23 +13,48 @@ struct PersonalInformation
     int Age;
 };
 
-void SortVector(vector<int> VN);
+void SortVector(vector<int> &VN);
 
 int main()
 {
+    vector<PersonalInformation> ContactVector;
+    
     vector<int> VectorNumber;
     int UserNumber;
     
-    for(int i = 0; i < 10; i++)
+    for(int i = 0; UserNumber != 1; i++)
     {
         cin >> UserNumber;
         VectorNumber.push_back(UserNumber);
     }
     
     SortVector(VectorNumber);
+    
+    cout << "stop";
 }
 
-void SortVector(vector<int> VN)
+void SortVector(vector<int> &VN)
 {
+    //cout << VN.size();//returns number of elements in vector
     
+    bool swapp = true;
+    
+    while(swapp)
+    {
+        swapp = false;
+        
+        for (size_t i = 0; i < VN.size()-1; i++)
+        {
+            if (VN[i] > VN[i+1])
+            {
+                VN[i] += VN[i+1];
+                
+                VN[i+1] = VN[i] - VN[i+1];
+                
+                VN[i] -= VN[i+1];
+                
+                swapp = true;
+            }
+        }
+    }
 }
